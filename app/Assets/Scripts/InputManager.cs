685f8e39b4d4a7c12d9ada01020aaaf0e8da276a
Snowman8 <python8taabo@gmail.com>
2015-01-18 01:39:41 +0900

    Added Input Manager

---
+++ app/Assets/Scripts/InputManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+    Vector2 slideStartPosition;
+    Vector2 prevPosition;
+    Vector2 delta = Vector2.zero;
+    bool moved = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    // スライド開始地点
+        if (Input.GetButtonDown("Fire1"))
+            slideStartPosition = GetCursorPosition();
+
+        // 画面の1割以上移動させたらスライド開始と判断する
+        if (Input.GetButton("Fire1"))
+        {
+            if (Vector2.Distance(slideStartPosition, GetCursorPosition()) >= (Screen.width * 0.1f))
+                moved = true;
+        }
+
+        // スライド操作が終了したか
+        if (!Input.GetButtonUp("Fire1") && !Input.GetButton("Fire1"))
+            moved = false;
+
+        // 移動量を求める
+        if (moved)
+            delta = GetCursorPosition() - prevPosition;
+        else
+            delta = Vector2.zero;
+
+        // カーソル位置を更新。
+        prevPosition = GetCursorPosition();
+	}
+
+
+    // クリックされたか
+    public bool Clicked()
+    {
+        if (!moved && Input.GetButtonUp("Fire1"))
+            return true;
+        else
+            return false;
+    }
+
+    // スライド時のカーソルの移動量
+    public Vector2 GetDeltaPosition()
+    {
+        return delta;
+    }
+
+    // スライド中か
+    public bool Moved()
+    {
+        return moved;
+    }
+
+
+    private Vector2 GetCursorPosition()
+    {
+        return Input.mousePosition;
+    }
+}
