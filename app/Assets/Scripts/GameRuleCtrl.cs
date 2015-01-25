6dea1c2419968a980bb7a995ae107a748e9fdcc3
Snowman8 <python8taabo@gmail.com>
2015-01-25 16:05:05 +0900

    タイマー機能

---
+++ app/Assets/Scripts/GameRuleCtrl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameRuleCtrl : MonoBehaviour {
+
+    // 残り時間　
+    public float timeRemaining = 5.0f * 60.0f;
+    // ゲームオーバーフラグ
+    public bool gameOver = false;
+    // ゲームクリア
+    public bool gameClear = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeRemaining -= Time.deltaTime;
+        // 残り時間がなくなったらゲームオーバー
+        if (timeRemaining <= 0.0f)
+            GameOver();
+	}
+
+    private void GameOver()
+    {
+        Debug.Log("GameOver");
+        gameOver = true;
+    }
+
+
+    public void GameClear()
+    {
+        Debug.Log("GameClear");
+        gameClear = true;
+    }
+}
