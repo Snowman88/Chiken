e1d7094f2790f8df239686dd5f61d77e8a5cdf4e
Snowman8 <python8taabo@gmail.com>
2015-01-25 12:26:15 +0900

    Coin Collider

---
+++ app/Assets/Scripts/CharacterStatus.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterStatus : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void GetItem()
+    {
+        Debug.Log("GET ITEM");
+    }
+}
---
+++ app/Assets/Scripts/DropCoin.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropCoin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        // player
+        if (other.tag != "Player")
+            return;
+
+        // Coin 処理
+        CharacterStatus status = other.GetComponent<CharacterStatus>();
+        status.GetItem();
+
+        // 消滅
+        Destroy(gameObject);
+    }
+}
